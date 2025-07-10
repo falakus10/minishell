1f4659e069f6b528f22d0943e034362686fa8fd9
Falakus <falak02@gmail.com>
2025-07-10 17:46:38 +0300

    expanderda değişiklik yapıldı

--- src/expander.c
+++ src/expander.c
@@ -36,84 +36,144 @@ char *env_value(char **env, const char *key)
  * – env_val, token  : malloc’lı  (fonksiyon sonunda serbest bırakılır)
  * Sonuç : malloc’lı yeni string  (çağıran free edecektir)
  */
+int find_index(const char *haystack, const char *needle)
+{
+	char *ptr;
+
+	ptr = ft_strnstr(haystack, needle, ft_strlen(haystack));
+	if (ptr == NULL)
+		return (-1);
+	return (ptr - haystack);  // pointer farkı → index
+}
+
 char *ft_strjoin_free(char *env_val, char *token, const char *key)
 {
-    size_t i;
-	size_t j;
-	size_t k;
-    size_t key_len;
-    size_t val_len;
-    size_t new_len;
+	int	i;
+	int	j;
+	int k;
+	int	new_len;
+	char *new_token;
 
 	i = 0;
 	j = 0;
 	k = 0;
-	key_len = ft_strlen(key);
-	val_len = ft_strlen(env_val);
+	if (!token || !env_val)
+		return (NULL);
+	new_len = ft_strlen(token) - (ft_strlen(key) + 1) + ft_strlen(env_val);
+	new_token = malloc(new_len + 1);
+	if (!new_token)
+	{
+		free(token);
+		free(env_val);
+		return (NULL);
+	}
+	i = find_index(token, key);
+	if (i == (-1))
+		return (NULL);
+	if (token[i - 1] == '$')
+	{
+		while ((i - 1) > j)
+		{
+			new_token[j] = token[j];
+			j++;
+		}
+		while(env_val[k])
+			new_token[j++] = env_val[k++];
+		i = i + ft_strlen(key);
+		while (token[i])
+			new_token[j++] = token[i++];
+		new_token [j++] = '\0';
+	}
+	else
+	{
+		i = find_index(token + i, key);
+		if (i == (-1))
+			return (NULL);
+		while ((i - 1) > j)
+		{
+			new_token[j] = token[j];
+			j++;
+		}
+		while(env_val[k])
+			new_token[j++] = env_val[k++];
+		i = i + ft_strlen(key);
+		while (token[i])
+			new_token[j++] = token[i++];
+		new_token [j++] = '\0';
+	}
+	return (new_token);
+}
 
-    if (!token || !env_val)
-        return (NULL);
-
-    new_len = ft_strlen(token) - (key_len + 1) + val_len; /* Yeni uzunluk = eski – ($+KEY) + VALUE */
-    char *new_token = malloc(new_len + 1); /* +1 ⇒ '\0' */
-
-    if (!new_token)
-    {
-        free(token);
-        free(env_val);
-        return (NULL);
-    }
-
-	while (token[i] && token[i] != '$')  /* '$' işaretine kadar kopyala */
-		new_token[k++] = token[i++];
-	if (token[i] == '$') /* '$' + KEY yerine VALUE kopyala */
-    {
-        i++; /* '$' atla */
-		while (j < val_len) /* VALUE kopyası */
-			new_token[k++] = env_val[j++];
-        i += key_len; /* KEY'i atla */
-    }
-
-	while (token[i]) /* Geri kalan SUFFIX'i kopyala */
-		new_token[k++] = token[i++];
-
-    new_token[k] = '\0';
-    free(token); /* eski token */
-    free(env_val); /* env_val malloc'lı idi → serbest */
-    return (new_token);
+int special_ch_check(char c)
+{
+	if (c >= '1' && c <= '9')
+		return (1);
+	else if (c == '#')
+		return (1);
+	else if (c == '*')
+		return (1);
+	else if (c == '@')
+		return (1);
+	else if (c == '-')
+		return (1);
+	else if (c == '!')
+		return (1);
+	else
+		return (0);
 }
 
 void expander(t_lexer_list *list, char **env)
 {
-    t_lexer_list	*temp;
-    size_t i;
-	size_t start;
-	char *env_key;
-	char *env_val;
-	char ch;
+	t_lexer_list	*temp;
+	size_t	i;
+	size_t	start;
+	char	*env_key;
+	char	*env_val;
+	char	ch;
 
-    temp = list;
-    i = 0;
+	temp = list;
+	i = 0;
 	start = 0;
-    while (temp != NULL) // her bir node 'u geziyoruz, tokenlarını kontrol ediyoruz
-    {	
+	while (temp != NULL) // her bir node 'u geziyoruz, tokenlarını kontrol ediyoruz
+	{	
 		i = 0;
-        while(temp->token[i] != '\0') //burda eski tokenla başlıyorum aşağıda aynı token ı freeliyorum sorun olabilir !! //tırnak görünce tırnağı atlasın. Çift tırnak içindeki expand durumunu ayrı bir yerde işleyeceğim.
+		while(temp->token[i] != '\0') //burda eski tokenla başlıyorum aşağıda aynı token ı freeliyorum sorun olabilir !! //tırnak görünce tırnağı atlasın. Çift tırnak içindeki expand durumunu ayrı bir yerde işleyeceğim.
 		{
-			if (temp->token[i] == '$') //$1SER kısmını da ayrı kontrol edicem
+			if (temp->token[i] == '$') //Literalde $1SER kısmını da kontrol ediyoruz
 			{
-				i++;
+				i++; //$?
+				if (special_ch_check(temp->token[i]) == 1)
+				{
+					while (special_ch_check(temp->token[i]))
+						i++;
+					while(temp->token[i] != '\0' && (is_valid_ch(temp,i))) 
+						i++;
+					continue;
+				}			//U
 				start = i; // $ işaretinden sonraki karakter başlangıç indexi olacak
 				while(temp->token[i] != '\0' && (is_valid_ch(temp,i)))  //inputun sonuna gelmediğimiz sürece ve geçerli bir karakter olduğu sürece devam
 					i++;  //Variable'ın key'inin uzunluğunu alıyoruz
 				env_key = ft_substr(temp->token, start, i - start); // $ işaretinden sonraki karakterden başlayarak geçerli karaktere kadar olan kısmı alır
 				env_val = env_value(env, env_key); //env'de bu key var mı diye bakar ve varsa değerini alır
+				if (temp->token[0] == '$' && temp->token[1] == '\0')
+				{
+					temp->token = malloc(sizeof(char) * 2); // Eğer sadece $ varsa, token'ı sadece $ olarak ayarla
+					temp->token[0] = '$';
+					temp->token[1] = '\0';
+					free(env_key);
+					free(env_val);
+					if(temp->next != NULL)
+						temp = temp->next; // Eğer sadece $ varsa, sonraki token'a geç
+					else
+						return; // Eğer sadece $ varsa, son token ise fonksiyondan çık //listenin son elemanı demek , return demek doğru mu ?? 
+					continue; // Eğer sadece $ varsa, token'ı sadece $ olarak ayarla ve sonraki token'a geç
+				}
 				temp->token = ft_strjoin_free(env_val, temp->token, env_key);//env_value'den dönen değeri token'in bulunduğu yere koyar
 				free(env_key);
 				i = 0;
 				continue; // $ işaretinden sonra gelen karakteri genişlettik, token'a yazdık devam ediyoruz
 			}
-			if (temp->token[i] == '\'' || temp->token[i] == '\"') //eğer tırnak varsa tırnağı atla, tırnak içini ayrıca kontrol edicez
+			if (temp->token[i] == '\'') //eğer tırnak varsa tırnağı atla, tırnak içini ayrıca kontrol edicez //else if mi 
 			{
 				ch = temp->token[i];
 				i++;
@@ -123,9 +183,29 @@ void expander(t_lexer_list *list, char **env)
 					i++;
 				continue;
 			}
+			if (temp->token[i] == '\"')    // Çift tırnak içi genişletiliyor. //Çift tırnak içinde $1SER kısmını da kontrol edicez
+			{
+				i++;
+				while (temp->token[i] != '\0' && temp->token[i] != '\"')
+				{
+					if(temp->token[i] == '$')
+					{
+						i++;
+						start = i; 
+						while(temp->token[i] != '\0' && (is_valid_ch(temp,i)))
+							i++;
+						env_key = ft_substr(temp->token, start, i - start);
+						env_val = env_value(env, env_key);
+						temp->token = ft_strjoin_free(env_val, temp->token, env_key);
+						free(env_key);
+						i = 1;
+					}
+					i++;
+				}
+			}
 			if(temp->token[i] != '$' && temp->token[i] != '\'' && temp->token[i] != '\"') //eğer $ işareti, tırnak veya çift tırnak değilse
 				i++;
 		}
 		temp = temp->next; //sonraki token'e geç
-    }
+	}
 }
--- src/lexer.c
+++ src/lexer.c
@@ -7,11 +7,7 @@ int	take_word(const char *input, int i)
 	len = 1;
 	while (input[i + 1] != ' ' && input[i + 1] != '\t' && input[i + 1] != '\0')
 	{
-		if (input[i] == '\\' && (input[i + 1] == '\'' || input[i + 1] == '\"'))
-		{
-			len++;
-		}
-		else if (is_meta(input, i + 1) != 0 || input[i + 1] == '\'' || input[i
+		if (is_meta(input, i + 1) != 0 || input[i + 1] == '\'' || input[i
 			+ 1] == '\"')
 			break ;
 		else if (input[i + 1] == '|' || input[i + 1] == '<' || input[i
@@ -25,34 +21,29 @@ int	take_word(const char *input, int i)
 
 int  quote_len(const char *input, int start, char delim)
 {
-	int len = 1;                  /* açılış tırnağını da saysın   */
-	int i   = start + 1;
+    int len = 1;                  /* açılış tırnağını da saysın   */
+    int i   = start + 1;
 
-	while (input[i] && input[i] != delim)
-	{
-		if (input[i] == '\\' && (input[i + 1] == '\'' || input[i + 1] == '\"'))
-		{
-			i++;
-			len++;
-		}
-		++len;                    /* tırnak içindeki karakterler  */
-		++i;
-	}
+    while (input[i] && input[i] != delim)
+    {
+        ++len;                    /* tırnak içindeki karakterler  */
+        ++i;
+    }
 
-	if (input[i] == '\0')         /* kapanış yok → hata           */
-		ft_error();
+    if (input[i] == '\0')         /* kapanış yok → hata           */
+        ft_error();
 
-	return len;                   /* kapanış tırnağına kadarki uzunluk */
+    return len;                   /* kapanış tırnağına kadarki uzunluk */
 }
 
 int is_quote(const char *input, int i)
 {
-	if (input[i] == '\'')
-		return quote_len(input, i, '\'');   /* tek tırnak */
-	else if (input[i] == '\"')
-		return quote_len(input, i, '\"');   /* çift tırnak */
-	else
-		return 0;                           /* tırnak değil */
+    if (input[i] == '\'')
+        return quote_len(input, i, '\'');   /* tek tırnak */
+    else if (input[i] == '\"')
+        return quote_len(input, i, '\"');   /* çift tırnak */
+    else
+        return 0;                           /* tırnak değil */
 }
 
 int	is_meta(const char *input, int i)

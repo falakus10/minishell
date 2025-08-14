8f2f831b02127031c7725162035c7917b3db6ad6
Ferhat Alakuş <falakus@k1m15s01.42istanbul.com.tr>
2025-08-14 17:05:43 +0300

    sinyaller normu yapıldı

--- src/signal.c
+++ src/signal.c
@@ -1,19 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falakus <falakus@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/14 17:02:13 by falakus           #+#    #+#             */
+/*   Updated: 2025/08/14 17:03:23 by falakus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	handle_sigint(int sig)
 {
 	(void)sig;
 	exit(130);
-}
-
-static void	suppress_output(void)
-{
-	struct termios	termios_p;
-
-	if (tcgetattr(0, &termios_p) != 0)
-		perror("Minishell: tcgetattr");
-	if (tcsetattr(0, 0, &termios_p) != 0)
-		perror("Minishell: tcsetattr");
 }
 
 static void	ctrl_d(int sig)
@@ -28,17 +30,17 @@ static void	ctrl_c(int sig)
 	(void)sig;
 	if (g_signal == 2)
 	{
-		write(1, "\n",1);  // ^C bastır
+		write(1, "^C\n", 1);
 	}
 	else if (g_signal == 3)
 	{
-		write(1, "\n",1);  // ^C bastır
+		write(1, "\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 	}
 	else
 	{
-		write(1, "\n", 1);  // ^C bastır
+		write(1, "\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
@@ -48,12 +50,11 @@ static void	ctrl_c(int sig)
 
 void	handle_signal(void)
 {
-	suppress_output();
 	signal(SIGINT, ctrl_c);
 	signal(SIGQUIT, SIG_IGN);
 }
 
-void setter_signal(int sig)
+void	setter_signal(int sig)
 {
 	if (sig == 0)
 	{
